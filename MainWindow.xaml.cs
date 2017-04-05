fed339a568ac57bc4f513b23eeb1b15f164de0ff
Rem <Rem.Kolomna@gmail.com>
2017-04-05 16:08:12 +0300

    add xml export

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
@@ -16,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace Poster
 {
@@ -23,6 +25,13 @@ namespace Poster
     {
         public string FirmID { get; set; }
         public string BarCode { get; set; }
+    }
+
+    public class RpoStatus
+    {
+        public string BarCode { get; set; }
+        public string Status { get; set; }
+        public string DateOper { get; set; }
     }
 
     /// <summary>
@@ -34,6 +43,8 @@ namespace Poster
         {
             InitializeComponent();
         }
+
+        private string _ticketId;
 
         private void Log(string Text)
         {
@@ -123,7 +134,7 @@ namespace Poster
             }            
         }
 
-        private void ExtractResponse(string ticketId)
+        private List<RpoStatus> ExtractResponse(string ticketId)
         {
             var Url = "https://tracking.russianpost.ru/fc";
             var request = (HttpWebRequest)WebRequest.Create(Url);
@@ -152,6 +163,8 @@ namespace Poster
             {
                 soapEnvelopeXml.Save(stream);
             }
+
+            var Result = new List<RpoStatus>();
             using (WebResponse response = request.GetResponse())
             {
                 var ResponseXml = new XmlDocument();
@@ -159,14 +172,31 @@ namespace Poster
                 XmlNamespaceManager manager = new XmlNamespaceManager(ResponseXml.NameTable);
                 manager.AddNamespace("S", "http://schemas.xmlsoap.org/soap/envelope/");
                 manager.AddNamespace("ns2", "http://fclient.russianpost.org/postserver");
+                manager.AddNamespace("ns3", "http://fclient.russianpost.org");
 
-                // TODO parse response
-                var Node = ResponseXml.SelectSingleNode("//S:Envelope//S:Body/ns2:ticketResponse/value", manager);
-                if (Node != null)
+                var ErrorNode = ResponseXml.SelectSingleNode("//S:Envelope//S:Body//ns2:ticketResponse/error", manager);
+                if (ErrorNode != null)
                 {
-                    
+                    Log($"Ошибка исполнения запроса: ${ErrorNode.Attributes["ErrorName"]}");
+                }
+                var List = ResponseXml.SelectNodes("//S:Envelope//S:Body/ns2:answerByTicketResponse/value//ns3:Item", manager);
+                if (List.Count > 0)
+                {
+                    for (var k = 0; k < List.Count; ++k)
+                    {
+                        var Item = List.Item(k);
+                        var Status = new RpoStatus();
+                        Status.BarCode = Item.Attributes["Barcode"].Value;
+                        if (Item.LastChild != null)
+                        {
+                            Status.Status = Item.LastChild.Attributes["OperName"].Value;
+                            Status.DateOper = Item.LastChild.Attributes["DateOper"].Value;
+                            Result.Add(Status);
+                        }                        
+                    }
                 }
             }
+            return Result;
         }
 
         private void ProcessFile(string fileName)
@@ -175,9 +205,10 @@ namespace Poster
             Log($"Файл ${fileName} импортирован");
             Log($"Найдено ${Codes.Count} РПО кодов");
             var TicketId = SendRequest(Codes);
-            if (TicketId != null)
+            this._ticketId = TicketId;
+            if (!string.IsNullOrEmpty(TicketId))
             {
-
+                Log($"Получен код запроса {TicketId}");
             }
         }
 
@@ -191,6 +222,44 @@ namespace Poster
             {
                 ProcessFile(Dialog.FileName);
             }            
+        }
+
+        private void ExportToXml(List<RpoStatus> items, string fileName)
+        {
+            var Doc = new XElement("RPO");
+            foreach (var Item in items)
+            {
+                var Elem = new XElement("BARCODE");
+                Elem.Add(new XElement("CODE", Item.BarCode));
+                Elem.Add(new XElement("STATUS", Item.Status));
+                Elem.Add(new XElement("DATE", Item.DateOper));
+                Doc.Add(Elem);
+            }
+            Doc.Save(fileName);
+        }
+
+        private void ExportItems(List<RpoStatus> items)
+        {
+            SaveFileDialog SaveDialog = new SaveFileDialog();
+            SaveDialog.Filter = "Xml|*.xml";
+            SaveDialog.Title = "Save an Image File";
+            SaveDialog.ShowDialog();
+            if (SaveDialog.FileName != "")
+            {
+                ExportToXml(items, SaveDialog.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (this._ticketId != null) {
+                var Items = ExtractResponse(this._ticketId);
+                ExportItems(Items);
+            }
+            else
+            {
+                Log($"Отсутствеует код запроса. Откройте файл для его получения.");
+            }
         }
     }
 }
